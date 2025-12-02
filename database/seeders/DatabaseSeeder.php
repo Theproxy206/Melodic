4c5c470ac8b35f7c0566ff1012479147442bdf75
tproxy <soberanoparedes@gmail.com>
2025-12-01 22:20:57 -0600

    Actualización de seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LabelSeeder::class,
             SubscriptionSeeder::class,
+            AdminSeeder::class,
         ]);
 
         User::create([
